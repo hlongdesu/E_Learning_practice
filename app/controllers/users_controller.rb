5a3c7c166ea35ceac2092add0c421dbf773f6707
bangpv97 <bangpv97@gmail.com>
2019-06-12 16:37:39 +0700

    Signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  before_action :load_user, only: %i(show)
+
+  def new
+    @user = User.new
+  end
+
+  def show; end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "greeting"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation, :avatar
+  end
+
+  def load_user
+    return @user if @user = User.find_by(id: params[:id])
+    flash.now[:danger] = t "user_not_found"
+    redirect_to root_path
+  end
+end
