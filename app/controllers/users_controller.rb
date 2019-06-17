518a98f5d545ee7372e15427a99e111f6e4e124f
hlongdesu <hoanglong2711997@gmail.com>
2019-06-17 15:13:31 +0700

    CRUD Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: %i(show)
+  before_action :logged_in_user, except: %i(new show create)
+  before_action :load_user, except: %i(new index create)
+  before_action :correct_user, only: %i(edit update)
+
+  def index
+    @users = User.paginate page: params[:page],
+      per_page: Settings.per_page
+  end
 
   def new
     @user = User.new
   end
 
-  def show; end
+  def show
+    return @user if @user = User.find_by(id: params[:id])
+    flash[:danger] = t "errors.user_not_found"
+    redirect_to root_path
+  end
 
   def create
     @user = User.new(user_params)
@@ -18,11 +29,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "profile_updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation, :avatar
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "errors.please_login"
+    redirect_to login_path
+  end
+
+  def correct_user
+    redirect_to root_path unless current_user? @user
   end
 
   def load_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_081151) do
+ActiveRecord::Schema.define(version: 2019_06_17_021957) do
 
   create_table "activities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2019_06_14_081151) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
   create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
